4a0c6f28e829228545f8261fba51c76633950c65
Wilson <wilsonkcarpenter@gmail.com>
2015-01-30 10:20:10 -0800

    Got the dynamic list additions to work in MainActivity

--- app/src/main/java/com/dasshoppinglist/dasshoppinglist/MainActivity.java
+++ app/src/main/java/com/dasshoppinglist/dasshoppinglist/MainActivity.java
@@ -7,40 +7,36 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.app.ListActivity;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
+    ArrayList<String> listItems;
+    //string adapter to handle listview
+    ArrayAdapter<String> adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+        listItems = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        setListAdapter(adapter);
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        switch (id) {
-            case R.id.calcMenuButton:
-                Intent intent = new Intent(getApplicationContext(), CalcActivity.class);
-                startActivity(intent);
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    /**
+     * Takes text from the textbox and adds it to the listview
+     * @param v
+     */
+    public void addItems(View v) {
+        String listName = ((EditText)findViewById(R.id.addListTextBox)).getText().toString();
+        listItems.add(listName);
+        adapter.notifyDataSetChanged();
+        ((EditText)findViewById(R.id.addListTextBox)).setText("");
     }
 
     public void list_click(View v) {
@@ -52,9 +48,5 @@ public class MainActivity extends ActionBarActivity {
         Intent intent = new Intent(getApplicationContext(), CalcActivity.class);
         startActivity(intent);
     }
-    //just for testing
-    public void listClick(View v) {
-        Intent intent = new Intent(getApplicationContext(), MainListActivity.class);
-        startActivity(intent);
-    }
+
 }

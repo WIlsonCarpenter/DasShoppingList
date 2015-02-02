060f1aecbd491454ca2bcb4d6169729f92276b49
Wilson <wilsonkcarpenter@gmail.com>
2015-02-01 21:10:23 -0800

    Added a basic spinner for category selection in item details

--- app/src/main/java/com/dasshoppinglist/dasshoppinglist/ItemDetailsActivity.java
+++ app/src/main/java/com/dasshoppinglist/dasshoppinglist/ItemDetailsActivity.java
@@ -1,24 +1,43 @@
 package com.dasshoppinglist.dasshoppinglist;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.ConsoleMessage;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 
 public class ItemDetailsActivity extends ActionBarActivity {
+    String[] categories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
+
+/*        NumberPicker np = (NumberPicker)findViewById(R.id.quantity);
+        String[] nums = new String[300];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.toString(i);
+        }
+        np.setMinValue(1);
+        np.setMaxValue(300);
+        np.setWrapSelectorWheel(false);
+        np.setDisplayedValues(nums);
+        np.setValue(1);*/
+
+        //initialize category array with values from strings.xml
+        categories = getResources().getStringArray(R.array.categoryArray);
+        Spinner catSpinner = (Spinner) findViewById(R.id.categorySpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, categories);
+        catSpinner.setAdapter(adapter);
 
         //---get the Bundle object passed in---
         Bundle bundle = getIntent().getExtras();
@@ -26,7 +45,8 @@ public class ItemDetailsActivity extends ActionBarActivity {
         //---get the data using the getString()---
         ((EditText)findViewById(R.id.itemName1)).setText(bundle.getString("itemName"));
         ((EditText)findViewById(R.id.priceField)).setText( Double.toString(bundle.getDouble("txt_price")) );
-        ((EditText)findViewById(R.id.quantity)).setText(Integer.toString(bundle.getInt("itemQty")));
+        ((EditText)findViewById(R.id.quantity)).setText((Integer.toString(bundle.getInt("itemQty"))));
+
 
     }
 
@@ -71,6 +91,9 @@ public class ItemDetailsActivity extends ActionBarActivity {
         extras.putString("itemName", ((EditText) findViewById(R.id.itemName1)).getText().toString());
         extras.putDouble("txt_price", Double.parseDouble(((EditText) findViewById(R.id.priceField)).getText().toString()));
         extras.putInt("itemQty", Integer.parseInt(((EditText) findViewById(R.id.quantity)).getText().toString()));
+    //    extras.putInt("itemQty", ((NumberPicker) findViewById(R.id.quantity)).getValue());
+
+
         //---set the data to pass back---
         //data.setData(Uri.parse(txt_price.getText().toString()));
         //data.setData(Uri.parse("junk"));
